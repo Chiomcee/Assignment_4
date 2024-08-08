1a1d61c48d047c6955063abf017dd65874e7df7d
Chioma Igwe <cigwe072013@gmail.com>
2024-08-08 03:21:24 +0100

    initial commit

---
+++ data_types.py
@@ -0,0 +1,54 @@
+# Integer
+x = 30
+type_x = type(x)
+print(f"The value of x is {x}, and the data type of x is {type_x}")
+
+# Float
+y = 30.5
+type_y = type(y)
+print(f"The value of y is {y}, and the data type of y is {type_y}")
+
+# String
+z = "Hello, World!"
+type_z = type(z)
+print(f"The value of z is '{z}', and the data type of z is {type_z}")
+
+# Boolean
+a = False
+type_a = type(a)
+print(f"The value of a is {a}, and the data type of a is {type_a}")
+
+# List
+b = ["apple", "banana", "cherry", "pear", "guava"]
+type_b = type(b)
+print(f"The value of b is {b}, and the data type of b is {type_b}")
+
+# Tuple
+c = ("apple", "banana", "cherry", "pear", "guava")
+type_c = type(c)
+print(f"The value of c is {c}, and the data type of c is {type_c}")
+
+# Dictionary
+d = {"name": "John", "age": 36}
+type_d = type(d)
+print(f"The value of d is {d}, and the data type of d is {type_d}")
+
+# Set
+e = {"apple", "banana", "cherry", "pear", "guava"}
+type_e = type(e)
+print(f"The value of e is {e}, and the data type of e is {type_e}")
+
+#frozenset
+f = frozenset({"apple", "banana", "cherry", "pear", "guava"})
+type_f =type(f)
+print(f"The value of f is {f}, and the data type of f is {type_f}")
+
+# NoneType
+g = None
+type_g = type(g)
+print(f"The value of g is {g}, and the data type of g is {type_g}")
+
+# complex
+h = 1j
+type_h = type(h)
+print(f"The value of h is {h} and the data type of h is {type_h}")
---
+++ fullname.py
@@ -0,0 +1,9 @@
+# variable holding first name, middle name and last name.
+firstName = "Chioma"
+middleName ="Charity"
+lastName = "Igwe"
+
+# print full name
+fullName = f'{firstName} {middleName} {lastName}'
+
+print(fullName)
